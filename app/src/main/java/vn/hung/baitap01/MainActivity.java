3f478dc4e404fe1da43cc4e84968050a63050e11
Hùng Nguyễn <vanhung211104@gmail.com>
2025-01-17 08:18:52 +0700

    Hình bo tròn và tên - mssv

---
+++ app/src/main/java/vn/hung/baitap01/MainActivity.java
@@ -0,0 +1,28 @@
+package vn.hung.baitap01;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        de.hdodenhof.circleimageview.CircleImageView imageView1 = findViewById(R.id.imageView1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
