f9f142dda637629aaddc2ddda007563c9186b2fe
Hùng Nguyễn <vanhung211104@gmail.com>
2025-01-17 09:02:29 +0700

    Ẩn thanh tiêu đề

--- app/src/main/java/vn/hung/baitap01/MainActivity.java
+++ app/src/main/java/vn/hung/baitap01/MainActivity.java
@@ -2,6 +2,8 @@ package vn.hung.baitap01;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title not the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
         setContentView(R.layout.activity_main);
-        de.hdodenhof.circleimageview.CircleImageView imageView1 = findViewById(R.id.imageView1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
